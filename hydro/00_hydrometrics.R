d92eec839c5b8a10d3aeb55827aa7221d1ef3e0c
Tamara Harms <tkharms@alaska.edu>
2024-08-27 13:48:09 -0700

    summarize discharge anomalies

--- hydro/00_hydrometrics.R
+++ hydro/00_hydrometrics.R
@@ -9,6 +9,7 @@ library(googledrive)
 library(discharge)
 library(data.table)
 library(lubridate)
+library(zoo)
 
 ######################
 ### Discharge data ###
@@ -122,7 +123,9 @@ metrics.pl <- met.out.df %>% ggplot(aes(x = site, y = value)) +
 
 ggsave(metrics.pl, path = here("hydro", "plots"), file = "DFFT_metrics_25y.pdf", width = 10, height = 8, units = "in")
 
-## Summarize metrics over 4 years pre- and 4 years post-fire
+#################################################################
+### Summarize metrics over 4 years pre- and 4 years post-fire ###
+#################################################################
 ### ****Goals here- ave metrics pre- and post-fire, then sum of + anomalies post-fire-- filter flow and seas to 4 y pre & 4 y post fire first?
 # Metrics by year
 ####****Summarizing by calendar year... Switch to years in fire timeline or water years?***####
@@ -149,6 +152,12 @@ DFFTyr <- function(X.flow, X.seas, SiteName){
 
 met.out.y <- mapply(x = flow.list, y = seas.list, z = names(seas.list), FUN = function(x, y, z) DFFTyr(x, y, z))
 
+#met.out.y.df <- met.out.y %>%
+#  as_tibble(., rownames = "metric") %>%
+#  unnest(-metric) 
+#%>%
+#  pivot_longer(!metric, names_to = "site", values_to = "value")
+
 ## Try single site...need to get year as a column
 ## This works
 ss.flow <- flow.list[["07103700"]]
@@ -156,48 +165,51 @@ ss.seas <- seas.list[["07103700"]]
 
 ss.metrics <- DFFTyr(ss.flow, ss.seas, "07103700")
 
-# filter to pre-fire & filter to post-fire
-# Add column with element names (sites) to facilitate joining with site data
-met.out.df <- 
+## filter to pre-fire & filter to post-fire
+# Note change years(x) if revising pre-/post-fire window
+t.sites.pp <- t.sites %>% mutate(before.fire = igDate - years(4)) %>%
+                          mutate(after.fire = igDate + years(4)) %>%
+                          mutate(ybeffire = format(as.Date(before.fire, format = "%Y-%m-%d"), "%Y")) %>%
+                          mutate(yaftfire = format(as.Date(after.fire, format = "%Y-%m-%d"), "%Y"))
 
-t.sites$site <- flow.list[match(t.sites$usgs_site, names(flow.list))]
-###***This runs but is it doing anything?
-###*
-###*####Instead, run the fcn w/o the averaging step. Collapse to df, join ignition date, then average to pre- and post-fire intervals
+names(ss.metrics)[names(ss.metrics) == 'Site'] <- 'usgs_site'
+ss.metrics.f <- left_join(ss.metrics, t.sites.pp, by = "usgs_site") 
 
-df$value_l <- l[match(df$group, names(l))]
+###########################
+### High-flow anomalies ###
+###########################
+# sum of pre-fire and post-fire high-flow anomalies
 
-my.list %>% 
-  bind_rows(.id = "groups")
+## Extract positive anomalies
+panom <- lapply(seas.list, function(x) subset(x$signal, x$signal$resid.sig >= 0) )
 
-flow.list.pp <- mapply(function(x) merge(t.sites, x, by = "Let"), list(A, B), SIMPLIFY = F)
+# Join site IDs to anomalies
+panom <- Map(cbind, panom, usgs_site = names(panom))
+panom <- lapply(panom, function(x) left_join(x, t.sites.pp, by = "usgs_site"))
 
-mapply(function(x) merge(DF, x, by = "Let"), list(A, B), SIMPLIFY = F)
+# sum pre- and post-fire positive anomalies
+pre.sum <- panom %>% map(~ .x %>%
+                           filter(date >= before.fire & date <= igDate) %>%
+                           summarize(bpos = sum(resid.sig)))
 
-# Apply DFFT summary fcn to pre- and post-fire observations
-# remove rms.signal, rms.noise, snr- from function- these are long-term metrics only
-DFFT-st <- function(X.flow, X.seas, SiteName){
-  X.bl <- prepareBaseline(X.flow)
-  X.FPExt <- getFPExt(X.bl$resid.sig, X.flow$data$year)
-  X.HSAF <- getHSAF(X.bl$resid.sig, X.flow$data$year)
-  X.HSAM <- getHSAM(X.bl$resid.sig, X.flow$data$year)%>%
-    select(year, HSAM)
-  X.LSAF <- getLSAF(X.bl$resid.sig, X.flow$data$year)
-  X.LSAM <- getLSAM(X.bl$resid.sig, X.flow$data$year)%>%
-    select(year, LSAM)
-  X.NAA <- getNAA(X.bl$resid.sig, X.flow$data$year)
-  X.HFsigma <- sigmaHighFlows(X.flow) #Use X.HFsigma$sigma.hfb
-  X.LFsigma <- sigmaLowFlows(X.flow) #Use X.LFsigma$sigma.hfb
-  X.Zdays <- X.seas$signal%>% #Average number of zero flow days per year
-    group_by(year)%>%
-    summarize(Zdays=sum(discharge <0.1))
-  #Find average of each DFFT metric
-  X.fftavg <- bind_cols(Site=SiteName, HSAM=mean(X.HSAM$HSAM, na.rm=T),LSAM=mean(X.LSAM$LSAM, na.rm=T),
-                        HSAF=mean(X.HSAF$HSAF, na.rm=T), LSAF=mean(X.LSAF$LSAF, na.rm=T), FPExt=mean(X.FPExt$FPExt),
-                        NAA=mean(X.NAA$NAA), HFsigma=mean(X.HFsigma$sigma.hfb), LFsigma=mean(X.LFsigma$sigma.lfb))
-}
+post.sum <- panom %>% map(~ .x %>%
+                           filter(date >= igDate & date <= after.fire) %>%
+                           summarize(apos = sum(resid.sig)))
 
-# reshape to long, add column for full record, pre-, post- fire
+dates <- panom %>% map(~ .x %>%
+                         select(c("usgs_site", "event_id", "igDate", "before.fire", "after.fire")) %>%
+                         unique())
 
-## Extract anomalies
-# sum of pre-fire and post-fire high-flow anomalies (and net)
+# combine
+pre.sum.df <- bind_rows(pre.sum, .id = "usgs_site")
+post.sum.df <- bind_rows(post.sum, .id = "usgs_site")
+dates.df <- bind_rows(dates, .id = "usgs_site")
+
+pos.anom <- left_join(dates.df, pre.sum.df)
+pos.anom <- left_join(pos.anom, post.sum.df)
+
+###***other possibilities:
+###*days since fire of largest positive anomaly OR some fraction of cdf of net anomalies?
+###*number days with positive anomalies post-fire OR number days with + anomalies above some threshold (like 2*SD mean of + anomalies)
+###*Average time between positive anomalies above some threshold
+
